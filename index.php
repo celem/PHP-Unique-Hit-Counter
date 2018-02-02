ee45f757c88d3ff89c21513355e80c793fb1628e
celem <comer.edward@gmail.com>
2018-02-02 12:21:33 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,31 @@
 <?php
-$filename = 'counter.txt';
-$ip_filename = 'ip.txt';
+$filename = 'store/counter.txt';
+$ip_filename = 'store/ip.txt';
 
-function inc_count(){
+function get_ip()
+{
+	if(!empty($_SERVER['HTTP_CLIENT_IP']))
+	{
+		$ip_address = $_SERVER['HTTP_CLIENT_IP'];
+	}else if(!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+	{
+		$ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	}else{
+		$ip_address = $_SERVER['REMOTE_ADDR'];
+	}
+	return $ip_address;
+}
+
+function RecordUniqueVisitorCount(){
 	$ip = get_ip();
-	global $filename, $ip_filename;
-	
+	global $filename, $ip_filename, $current_visitor_Count;
+	$current_visitor_Count = (file_exists($filename)) ? file_get_contents($filename) : 0;
 	if(!in_array($ip, file($ip_filename, FILE_IGNORE_NEW_LINES))){
-		$current_value = (file_exists($filename)) ? file_get_contents($filename) : 0;
 		file_put_contents($ip_filename, $ip."\n", FILE_APPEND);
-		file_put_contents($filename, ++$current_value);
+		file_put_contents($filename, ++$current_visitor_Count);
 	}
 }
 
-function get_ip(){
-
-if(!empty($_SERVER['HTTP_CLIENT_IP'])){
-	$ip_address = $_SERVER['HTTP_CLIENT_IP'];
-}else if(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
-	$ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
-}else{
-	$ip_address = $_SERVER['REMOTE_ADDR'];
-}
-
-return $ip_address;
-}
-inc_count();
+      RecordUniqueVisitorCount();
+      echo $current_visitor_Count . " - ";
 ?>
